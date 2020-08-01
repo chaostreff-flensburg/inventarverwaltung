1d79af3fb618567bb96a338f735eb8eb0f38ec06
Alexander Eichhorn <alex@fl1rt.net>
2020-08-01 16:03:27 +0200

    Models Dir + Controller names

--- app/Http/Controllers/ItementityController.php
+++ app/Http/Controllers/ItementityController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Itementity;
+use App\Models\Itementity;
 
-class InventoryController extends Controller
+class ItementityController extends Controller
 {
     //
     public function index()
@@ -14,7 +14,16 @@ class InventoryController extends Controller
                             ->join('storagelocations', 'itementities.storagelocation_id', 'storagelocations.id')
                             ->select('itementities.*', 'items.name as itemname', 'storagelocations.name as storagename')
                             ->get();
-                                //dd($items);
-        return view('inventory.index', compact('items'));
+        return view('itementity.index', compact('items'));
+    }
+
+    public function createform()
+    {
+        return view('itementity.create');
+    }
+
+    public function create(Request $request)
+    {
+        //dd($request);
     }
 }
