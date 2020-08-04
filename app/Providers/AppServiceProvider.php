948c66aa014bdf8f7e02eb8be732fe72078d5ddf
Philipp Kübler <info@pkuebler.de>
2020-08-04 19:47:23 +0200

    force https on productions

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if ($this->app->environment('staging', 'production')) {
+            URL::forceScheme('https');
+        }
     }
 }
