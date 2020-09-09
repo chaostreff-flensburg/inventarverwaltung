0583222f5b6ba71dfc5cafe7f8ec1d81482bfc7d
Philipp Kübler <info@pkuebler.de>
2020-09-09 22:06:52 +0200

    change title

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,7 +4,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
-    <title>Title</title>
+    <title>Inventory</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Styles -->
